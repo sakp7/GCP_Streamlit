c6937699591a223a07aa651a79fe853ac5611a34
sakp7 <119031981+sakp7@users.noreply.github.com>
2023-01-08 21:45:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,154 @@
+import numpy as np
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+
+df = pd.read_csv("D:/project2/Loan_Data/train.csv")
+df.head()
+
+df['LoanAmount'] = df['LoanAmount'].fillna(df['LoanAmount'].mean())
+df['Credit_History'] = df['Credit_History'].fillna(df['Credit_History'].median())
+df.dropna(inplace=True)
+print(df.isnull().sum())
+print(df.shape)
+
+
+df['Loan_Status'].replace('Y',1,inplace=True)
+df['Loan_Status'].replace('N',0,inplace=True)
+
+
+
+
+df.Gender=df.Gender.map({'Male':1,'Female':0})
+df['Gender'].value_counts()
+
+df.Married=df.Married.map({'Yes':1,'No':0})
+df['Married'].value_counts()
+
+df.Dependents=df.Dependents.map({'0':0,'1':1,'2':2,'3+':3})
+df['Dependents'].value_counts()
+
+df.Education=df.Education.map({'Graduate':1,'Not Graduate':0})
+df['Education'].value_counts()
+
+df.Self_Employed=df.Self_Employed.map({'Yes':1,'No':0})
+df['Self_Employed'].value_counts()
+
+df.Property_Area=df.Property_Area.map({'Urban':2,'Rural':0,'Semiurban':1})
+df['Property_Area'].value_counts()
+
+print(df.head())
+
+
+
+
+X = df.iloc[1:542,1:12].values
+y = df.iloc[1:542,12].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3,random_state=0)
+
+model = LogisticRegression()
+model.fit(X_train,y_train)
+
+lr_prediction = model.predict(X_test)
+print('Logistic Regression accuracy = ', metrics.accuracy_score(lr_prediction,y_test))
+
+import streamlit as st
+
+
+
+
+
+
+def run():
+
+    st.title("Bank Loan Prediction using Machine Learning")
+
+    ## Account No
+    account_no = st.text_input('Account number')
+
+    ## Full Name
+    fn = st.text_input('Full Name')
+
+    ## For gender
+    gen_display = ('Female','Male')
+    gen_options = list(range(len(gen_display)))
+    gen = st.selectbox("Gender",gen_options, format_func=lambda x: gen_display[x])
+
+    ## For Marital Status
+    mar_display = ('No','Yes')
+    mar_options = list(range(len(mar_display)))
+    mar = st.selectbox("Marital Status", mar_options, format_func=lambda x: mar_display[x])
+
+    ## No of dependets
+    dep_display = ('No','One','Two','More than Two')
+    dep_options = list(range(len(dep_display)))
+    dep = st.selectbox("Dependents",  dep_options, format_func=lambda x: dep_display[x])
+
+    ## For edu
+    edu_display = ('Not Graduate','Graduate')
+    edu_options = list(range(len(edu_display)))
+    edu = st.selectbox("Education",edu_options, format_func=lambda x: edu_display[x])
+
+    ## For emp status
+    emp_display = ('Job','Business')
+    emp_options = list(range(len(emp_display)))
+    emp = st.selectbox("Employment Status",emp_options, format_func=lambda x: emp_display[x])
+
+    ## For Property status
+    prop_display = ('Rural','Semi-Urban','Urban')
+    prop_options = list(range(len(prop_display)))
+    prop = st.selectbox("Property Area",prop_options, format_func=lambda x: prop_display[x])
+
+    ## For Credit Score
+    cred_display = ('Between 300 to 500','Above 500')
+    cred_options = list(range(len(cred_display)))
+    cred = st.selectbox("Credit Score",cred_options, format_func=lambda x: cred_display[x])
+
+    ## Applicant Monthly Income
+    mon_income = st.number_input("Applicant's Monthly Income($)",value=0)
+
+    ## Co-Applicant Monthly Income
+    co_mon_income = st.number_input("Co-Applicant's Monthly Income($)",value=0)
+
+    ## Loan AMount
+    loan_amt = st.number_input("Loan Amount",value=0)
+
+    ## loan duration
+    dur_display = ['2 Month','6 Month','8 Month','1 Year','16 Month']
+    dur_options = range(len(dur_display))
+    dur = st.selectbox("Loan Duration",dur_options, format_func=lambda x: dur_display[x])
+
+    if st.button("Submit"):
+        duration = 0
+        if dur == 0:
+            duration = 60
+        if dur == 1:
+            duration = 180
+        if dur == 2:
+            duration = 240
+        if dur == 3:
+            duration = 360
+        if dur == 4:
+            duration = 480
+        features = [[gen, mar, dep, edu, emp, mon_income, co_mon_income, loan_amt, duration, cred, prop]]
+        print(features)
+        prediction = model.predict(features)
+        lc = [str(i) for i in prediction]
+        ans = int("".join(lc))
+        if ans == 0:
+            st.error(
+                "Hello: " + fn +" || "
+                "Account number: "+account_no +' || '
+                'According to our Calculations, you will not get the loan from Bank'
+            )
+        else:
+            st.success(
+                "Hello: " + fn +" || "
+                "Account number: "+account_no +' || '
+                'Congratulations!! you will get the loan from Bank'
+            )
+
+run()
